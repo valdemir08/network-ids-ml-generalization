eea4ab4d3982b890a3f204c397f070c53a94be04
Valdemir <valdemireverton08@gmail.com>
2026-04-19 14:16:08 -0300

    modificações relacionadas ao merge final do dataset

--- scripts/run_dataset_pipeline.py
+++ scripts/run_dataset_pipeline.py
@@ -1,7 +1,7 @@
 from src.pipelines.prepare_labels import prepare_labels
 from src.pipelines.extract_from_pcap import extract_from_pcap
 from src.pipelines.build_dataset import merge_flows_and_labels
-# estudar mudança no nome dessa função para evitar ambiguidades. talvez um merge_flows_and_labels...
+from src.pipelines.merge_dataset import merge_processed_dataset
 from src.configs.datasets import DATASETS
 from src.utils.select_dataset import select_dataset
 
@@ -14,6 +14,9 @@ if __name__ == "__main__":
         prepare_labels(dataset_name, scenario)
         extract_from_pcap(dataset_name, scenario)
         merge_flows_and_labels(dataset_name, scenario)
+
+    merge_processed_dataset(dataset_name)
+
 
 """
 if __name__ == "__main__":
--- src/configs/paths.py
+++ src/configs/paths.py
@@ -7,3 +7,4 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 DATASETS_DIR = PROJECT_ROOT / "datasets"
 INTERMEDIATE_DATA_DIR = DATASETS_DIR / "intermediate"
 PROCESSED_DATA_DIR = DATASETS_DIR / "processed"
+FINAL_DATA_DIR = DATASETS_DIR / "final"
